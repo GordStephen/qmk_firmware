552c3578bd509ff2f559faa73267abc193f49623
Gord Stephen <mail@gord.io>
2022-11-25 18:13:57 -0800

    [gs] Add Atreus62 keymap

---
+++ keyboards/atreus62/keymaps/gord/keymap.c
@@ -0,0 +1,39 @@
+// this is the style you want to emulate.
+// This is the canonical layout file for the Quantum project. If you want to add another keyboard,
+
+#include QMK_KEYBOARD_H
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _COLEMAK_DH 0
+#define _QWERTY 1
+#define _NAV 2
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_COLEMAK_DH] = LAYOUT(
+    KC_GRV,     KC_1,    KC_2,    KC_3,    KC_4,   KC_5,                                    KC_6,            KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+    KC_LBRC,    KC_Q,    KC_W,    KC_F,    KC_P,   KC_B,                                    KC_J,            KC_L,    KC_U,    KC_Y,    KC_QUOT, KC_RBRC,
+    KC_LSPO,    KC_A,    KC_R,    KC_S,    KC_T,   KC_G,                                    KC_M,            KC_N,    KC_E,    KC_I,    KC_O,    KC_RSPC,
+    KC_COMM,    KC_Z,    KC_X,    KC_C,    KC_D,   KC_V,                                    KC_K,            KC_H,    KC_MINS, KC_EQL,  KC_SCLN, KC_DOT,
+    KC_HOME,    KC_END,  KC_INS,  KC_LGUI, KC_DEL, KC_BSPC, LCTL_T(KC_ESC), RALT_T(KC_ENT), LT(_NAV,KC_SPC), KC_TAB,  KC_RGUI, KC_SLSH, KC_PGUP, KC_PGDN
+  ),
+
+  [_QWERTY] = LAYOUT(
+    _______,    _______, _______, _______, _______, _______,                                _______, _______, _______, _______, _______, _______,
+    _______,    _______, _______, KC_E   , KC_R   , KC_T   ,                                KC_Y   , KC_U   , KC_I   , KC_O   , _______, _______,
+    _______,    _______, KC_S   , KC_D   , KC_F   , _______,                                KC_H   , KC_J   , KC_K   , KC_L   , KC_P   , _______,
+    _______,    _______, _______, _______, KC_V   , KC_B   ,                                KC_N   , KC_M   , _______, _______, _______, _______,
+    _______,    _______, _______, _______, _______, _______,       _______, _______,        _______, _______, _______, _______, _______, _______
+  ),
+
+  [_NAV] = LAYOUT(
+    _______,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+    _______,    _______, _______, _______, _______, _______,                                _______, _______, _______, _______, _______, KC_F12,
+    _______,    _______, _______, _______, _______, _______,                                KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+    _______,    _______, _______, _______, _______, _______,                                _______, _______, _______, _______, _______, _______,
+    TG(_QWERTY),_______, _______, _______, _______, _______,       _______, _______,        _______, _______, _______, _______, _______, RESET
+  )
+
+};
