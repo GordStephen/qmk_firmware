497f244beb6914dd895e3dfe856e099d776a5c04
Gord Stephen <mail@gord.io>
2023-02-18 14:41:51 -0800

    [gs] Add Preonic keymap

---
+++ keyboards/preonic/keymaps/gordstephen/keymap.c
@@ -0,0 +1,67 @@
+/* Copyright 2015-2017 Jack Humbert
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+enum preonic_layers {
+  _BASE,
+  _FN,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Base (QWERTY)
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |   \  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |   =  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |   -  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |LShift|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ,  |RShift|
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |   [  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   ;  |  ]   |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Home |  End |  Ins |  Del |  Esc | BkSp | Space| Enter| Tab  |   /  | PgUp | PgDn |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_BASE] = LAYOUT_preonic_grid( \
+  KC_GRV,  KC_1,   KC_2,           KC_3,           KC_4,           KC_5,             KC_6,    KC_7,           KC_8,           KC_9,    KC_0,    KC_BSLS, \
+  KC_EQL,  KC_Q,   KC_W,           KC_E,           KC_R,           KC_T,             KC_Y,    KC_U,           KC_I,           KC_O,    KC_P,    KC_MINS, \
+  KC_LSPO, KC_A,   KC_S,           KC_D,           KC_F,           KC_G,             KC_H,    KC_J,           KC_K,           KC_L,    KC_QUOT, KC_RSPC, \
+  KC_LBRC, KC_Z,   KC_X,           KC_C,           KC_V,           KC_B,             KC_N,    KC_M,           KC_COMM,        KC_DOT,  KC_SCLN, KC_RBRC, \
+  KC_HOME, KC_END, LGUI_T(KC_INS), LT(_FN, KC_DEL), LCTL_T(KC_ESC), LALT_T(KC_BSPC),  KC_SPC,  RCTL_T(KC_ENT), LT(_FN, KC_TAB), KC_SLSH, KC_PGUP, KC_PGDN \
+),
+
+/* Function Layer
+ * ,-----------------------------------------------------------------------------------.
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      | MsUp |      |      |      |      |      |      |      | F12  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |MsLeft|MsDown|MsRght|      | Left | Down |  Up  | Right|      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      | Lclk | Rclk |      |      |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_FN] = LAYOUT_preonic_grid( \
+  KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F12,  \
+  KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  \
+)
+};
